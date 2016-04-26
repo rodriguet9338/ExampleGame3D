4bff122b0cafe4a312d4d0891b3d615f266185f1
rayrayc27 <rayrayc27@gmail.com>
2016-04-26 13:08:35 -0700

    scripts

---
+++ 3DGame/Assets/Scripts/kill.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class kill : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		other.gameObject.transform.Translate (0, 5, 5);
+	}
+}
