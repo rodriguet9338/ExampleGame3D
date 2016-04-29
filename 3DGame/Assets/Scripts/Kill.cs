cf0e3ec0cb078a1a88413348f6f7188a2e9c65d3
rodriguet9338 <trod888@gmail.com>
2016-04-29 12:04:23 -0700

    4-29
    kill script added

---
+++ 3DGame/Assets/Scripts/Kill.cs
@@ -0,0 +1,26 @@
+﻿public class kill : MonoBehaviour {
+	
+	public float x;
+	public float y;
+	public float z;
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter (Collider other) {
+		x = other.transform.position.x;
+		y = other.transform.position.y;
+		z = other.transform.position.z;
+		float movingX = -x;
+		float movingY = -1 * (y-10);
+		float movingZ = -1 * (z-10);
+		other.transform.Translate(movingX,movingY,movingZ);
+		
+		
+	}
